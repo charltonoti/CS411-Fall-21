aa893715f1e9a4f84a0c08003f6633e4493016b6
xingpeiz <91737900+xingpeiz@users.noreply.github.com>
2021-11-16 16:20:16 -0500

    Update stock_search.py

--- stock_search.py
+++ stock_search.py
@@ -9,10 +9,16 @@ import sys
 def search_stock(stock_name):
     # search a stock with its symbol utilize yahoo! finance API and return relative information.
     stock_data = yf.Ticker(stock_name)
-
-
     stock_infor = stock_data.info
     output_dic = {}
+    
+    if 'dayHigh' not in stock_name:
+        output_dic['txt'] = "Sorry, please reenter the symbol for the stock or try this link"
+        output_dic['linkToStockPage'] = f'https://finance.yahoo.com/quote/{stock_name}?p={stock_name}&.tsrc=fin-srch'
+        result = json.dumps(output_dic, indent = 4)
+        return result
+    
+
     output_dic['symbol'] = stock_name
     output_dic['name'] = stock_infor['longName']
     output_dic['currentPrice'] = stock_infor['currentPrice']
@@ -21,9 +27,9 @@ def search_stock(stock_name):
     output_dic['currency'] = stock_infor['currency']
     output_dic['recommendationKey'] = stock_infor['recommendationKey']
     output_dic['linkToStockPage'] = f'https://finance.yahoo.com/quote/{stock_name}?p={stock_name}&.tsrc=fin-srch'
-    stock_infor.json = json.dumps(output_dic, indent = 4)
+    result = json.dumps(output_dic, indent = 4)
     
-    return stock_infor.json
+    return result
     
 
     
