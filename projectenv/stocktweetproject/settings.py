58af057b91ba6c03004ce8be1b88468a8bf59bb8
Grace Yoon <43836126+gyoon18@users.noreply.github.com>
2021-11-17 18:58:04 -0500

    Update settings.py

--- projectenv/stocktweetproject/settings.py
+++ projectenv/stocktweetproject/settings.py
@@ -19,9 +19,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-d)1dh$*7fa=e+ce7okw7yjou9rrmod(jug=75!3+r^2j=*=uh0'
-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
