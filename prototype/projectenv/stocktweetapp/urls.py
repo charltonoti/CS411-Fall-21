98f5e4db8a5638f1157af18753921bc4beddaf45
Charlton <57676420+charltonoti@users.noreply.github.com>
2021-12-10 09:22:57 -0500

    Update urls.py

--- prototype/projectenv/stocktweetapp/urls.py
+++ prototype/projectenv/stocktweetapp/urls.py
@@ -5,6 +5,7 @@ from .import views
 
 urlpatterns = [
     path('search', views.search_stock, name = 'search_stock'),
-    path('',views.index, name = "index")
+    path('',views.index, name = "index"),
+    path('Sign in/ Sign up',views.login, name = "login")
    
 ]
